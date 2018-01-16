d71873089c32bc1e0ab265737189986a07d2a4b3
solo <cj@cachecats.com>
2018-01-16 18:14:14 +0800

    将第三方依赖提取到单独的文件中

---
+++ data/src/main/java/com/cachecats/data/Hah.java
@@ -0,0 +1,8 @@
+package com.cachecats.data;
+
+/**
+ * Created by solo on 2018/1/16.
+ */
+
+public class Hah {
+}
---
+++ data/src/main/java/com/cachecats/data/db/MeituanDB.java
@@ -0,0 +1,11 @@
+package com.cachecats.data.db;
+
+/**
+ * Created by solo on 2018/1/16.
+ */
+
+
+public class MeituanDB {
+    public final static String NAME = "MeituanDB";
+    public final static int VERSION = 1;
+}
